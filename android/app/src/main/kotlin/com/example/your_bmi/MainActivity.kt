bcae2465f3ce266f33eda0e2a54f9bb899c4d990
thisisdishang <dishangkumarrana@gmail.com>
2024-07-29 00:22:08 +0530

    Start BMI app

---
+++ android/app/src/main/kotlin/com/example/your_bmi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.your_bmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade800,
+        title: Center(
+          child: Text(
+            'YourBMI',
+            style: TextStyle(color: Colors.white, fontFamily: 'SpecialElite'),
+          ),
+        ),
+      ),
+      body: Text('Hello Users'),
+    );
+  }
+}
